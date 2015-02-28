18087597777cd9d4a2f5502807bb5592fd79c1d4
shotaro <shotaro@KannoShotaro.mshome.net>
2015-02-28 22:31:27 +0900

    ベタ書きをすこしなおしました
    足踏みし始めた感じです

--- utility/invalidateKeyControl/invalidateKeyControl.js
+++ utility/invalidateKeyControl/invalidateKeyControl.js
@@ -1,38 +1,57 @@
 // キーの無効化をコントロールします
 addEventListener('DOMContentLoaded', function() {
 
-	(function(KEY_CODE,disables){
-	/*		この関数で使うであろうプロパティです	*/
-	//	コマンド入力などする場合、これを見る感じです
-		// ユーザー操作状況の管理定義体
-		var state = {
-			keyIs		:	'',
-			clickIs		:	'',
-			targetKey	:	'',
-		};
+	(function(KEY_CODE,DISABLE_KEY_NAME){
+	/*		記述ルール的な	*/
+	//	危険な記述を禁止しますよ
+	"use strict";
 
-		//	危険な記述を禁止しますよ
-		"use strict";
-
-	/*		定義データから処理用データ生成です	*/
+	/*		受領データを処理用に整理します	*/
 		//	インターフェイスは配列にし、内部でオブジェクトにする予定
 
-		//	キーの有効・無効の設定です
-		var underControl = {};
-		for (var key in KEY_CODE) {
-			var code	=	KEY_CODE[key];
-			var	status	=	(disables[key] === undefined)?	'enable':	'disable';
+		//	キーの有効・無効の設定の作成と登録です
+		var _rule = {};
+		for (var name in KEY_CODE) {
+			var code	=	KEY_CODE[name];
+			var	status	=	(DISABLE_KEY_NAME[name] === undefined)?	'enable':	'disable';
 
-			underControl[code] = {
-				name	:	key,
+			_rule[code] = {
+				name	:	name,
 				state	:	status,
 			}
 		};
-		console.log(underControl);
 
+	/*		この関数で使うであろう管理するプロパティです	*/
+	//	コマンド入力などする場合、これを見る感じです
+		var systemProparty ={
+			// ユーザー操作状況の管理定義体
+			useState	:	{
+				downKeyIs		:	{
+					count	:	0,
+					name	:	'',
+				},
+				clickIs		:	{
+					count	:	0,
+					name	:	'',
+				},
+				count		:	{
+					up	:	function (ct){
+						return ct++;
+					}
+				},
+			},
+			//	有効・無効の定義体です
+			//	key 配下のオブジェクトは下記のように定義してください
+			//	keycode	.keyname
+			//			.enable or disable
+			rule	:{
+				key	:	_rule,
+			},
+		};
+	console.log(systemProparty.rule.key);
 
-	/*		コントロール系処理です。	*/
-	// 検知する操作にひもづくメソッドの登録です
+	/*		システムのコントロール系処理です。	*/
+	//	検知する操作にひもづくメソッドの登録です
 	var detection ={
 		click	:	function () {return isClicked() },
 		keydown	:	function () {return isDownKey() },
@@ -40,25 +59,25 @@ addEventListener('DOMContentLoaded', function() {
 	}
 
 	/*		処理系です。	*/
-	var test =0;
+	//	共通処理はやっぱりわけないとダメかも
 	function isClicked () {
 
-		console.log("クリック:"+test++);
+		console.log("クリック:");
 		window.event.returnValue = false;
 
 	}
 	function isDownKey () {
-		console.log("キーダウン:"+test++);
-		if (underControl[window.event.keyCode].state == 'disable') {
-			alert('今押したのは'+underControl[window.event.keyCode].name+'ですね？どやぁ');
+		console.log("キーダウン:");
+		if (systemProparty.rule.key[window.event.keyCode].state == 'disable') {
+			alert('今押したのは'+systemProparty.rule.key[window.event.keyCode].name+'ですね？どやぁ');
 
 			window.event.returnValue = false;
 		}
 	}
 	function isUpKey () {
 		window.event.returnValue = false;
-		console.log("キーアップ:"+test++);
-		if (underControl[window.event.keyCode].state == 'disable') {
+		console.log("キーアップ:");
+		if (systemProparty.rule.key[window.event.keyCode].state == 'disable') {
 			window.event.returnValue = false;
 		}
 	}
@@ -70,12 +89,12 @@ addEventListener('DOMContentLoaded', function() {
 		document.addEventListener(key,data, false);
 	}
 
-	})(KEY_CODE,disables)
+	})(KEY_CODE,DISABLE_KEY_NAME)
 
 });
 
 //	画面ごとに下記を書きます　インターフェイスの予定。配列にする予定
-var disables = {
+var DISABLE_KEY_NAME = {
 	'Z':'',
 	'BackSpace':'',
 };
